8dfed531ff8107802b64e8e2f0bf7decfc5b5a46
DESKTOP-246RQJI\ausal <ausalavert@gmail.com>
2023-01-16 16:03:04 +0100

    Update slider item

---
+++ src/api/slider-item/routes/slider-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * slider-item router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::slider-item.slider-item');
---
+++ src/api/slider-item/services/slider-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * slider-item service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::slider-item.slider-item');
