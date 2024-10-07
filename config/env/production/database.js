97de898a5b687dbd426556c218e0e40680a57fe8
aurore <aurore.salavert@freecaster.com>
2024-10-07 16:22:23 +0200

    Update database configuration for Heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'heroku_db_name'),
-      user: env('DATABASE_USERNAME', 'heroku_user'),
-      password: env('DATABASE_PASSWORD', 'heroku_password'),
-      ssl: {
-        rejectUnauthorized: false,
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        connection: env('DATABASE_URL'),
+      },
+      options: {
+        useNullAsDefault: true,
       },
     },
   },
