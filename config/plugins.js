90a5611497e6abee669d11743e6ff44eb8ee96a3
aurore <aurore.salavert@freecaster.com>
2024-10-07 16:32:04 +0200

    jwtSecret conf

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
     email: {
-      provider: 'YOUR_EMAIL_PROVIDER',
-      providerOptions: {
-        // Options for your email provider
-      },
-      settings: {
-        defaultFrom: 'hello@aurore-salavert.fr',
-        defaultReplyTo: 'hello@aurore-salavert.fr',
-      },
+        provider: 'YOUR_EMAIL_PROVIDER',
+        providerOptions: {
+            // Options for your email provider
+        },
+        settings: {
+            defaultFrom: 'hello@aurore-salavert.fr',
+            defaultReplyTo: 'hello@aurore-salavert.fr',
+        },
     },
-  });
-  
+    'users-permissions': {
+        config: {
+            jwtSecret: env('jwtSecret', 'DFeVO895pymJjKZcZtl0og=='),
+        },
+    },
+});
