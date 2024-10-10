1d39f8012cb93a6885c0d90a7b82eb550ce97874
Aurore S. <ausalavert@gmail.com>
2024-10-10 09:33:00 +0200

    Added: Strapi plugin to upload pictures on S3 Bucket

--- config/plugins.js
+++ config/plugins.js
@@ -11,7 +11,18 @@ module.exports = ({ env }) => ({
     },
     'users-permissions': {
         config: {
-            jwtSecret: env('jwtSecret', 'DFeVO895pymJjKZcZtl0og=='),
+            jwtSecret: env('jwtSecret', 'JWT_SECRET'),
+        },
+    },
+    upload: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
         },
     },
 });
