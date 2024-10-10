47a0084eb7b68d26ab945b967d01dd98ca612218
Aurore S. <ausalavert@gmail.com>
2024-10-10 11:29:35 +0200

    s3Options

--- config/plugins.js
+++ config/plugins.js
@@ -22,26 +22,26 @@ module.exports = ({ env }) => ({
     },
     upload: {
         config: {
-          provider: 'aws-s3',
-          providerOptions: {
-            s3Options: {
-              credentials: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
-              },
-              region: env('AWS_REGION'),
-              params: {
+            provider: 'aws-s3',
+            providerOptions: {
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                        secretAccessKey: env('AWS_ACCESS_SECRET'),
+                    },
+                    region: env('AWS_REGION'),
+                    params: {
+                        Bucket: env('AWS_BUCKET'),
+                    },
+                },
                 ACL: 'private',
                 signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-                Bucket: env('AWS_BUCKET'),
-              },
             },
             actionOptions: {
-              upload: {},
-              uploadStream: {},
-              delete: {},
+                upload: {},
+                uploadStream: {},
+                delete: {},
             },
-          },
         },
-      },
+    },
 });
